f13f010d34cbd39bb1702d77bd49503b0086a9c9
Mariusz Kunka <kunka.mariusz23@gmail.com>
2024-06-16 21:24:34 +0200

    Game loop

---
+++ Wisielec-praca_domowa/Wisielec-praca_domowa/game.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+class Game
+{
+public:
+	void OnInit();
+	void OnUpdate(float deltatime);
+	void OnShutdown();
+
+
+private:
+
+};
--- Wisielec-praca_domowa/Wisielec-praca_domowa/main.cpp
+++ Wisielec-praca_domowa/Wisielec-praca_domowa/main.cpp
@@ -1,10 +1,56 @@
 #include <iostream>
+#include "Game.h"
 
 using namespace std;
 
+void Initialize();
+
+void GetInput();
+
+void Update();
+
+void Render();
+
+void Shutdown();
+
+bool exitGame = false;
+
 int main()
 {
+	Game game;
+
+	Initialize();
+
+	while (exitGame)
+	{
+		GetInput();
+		Update();
+		Render();
+		
+	}
+
+	Shutdown();
 
 
 	return 0;
+}
+
+void Initialize()
+{
+}
+
+void GetInput()
+{
+}
+
+void Update()
+{
+}
+
+void Render()
+{
+}
+
+void Shutdown()
+{
 }
